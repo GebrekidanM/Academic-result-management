b3eb763bb39453bd2a1176a112aa219c5ac571bb
GebrekidanM <designerkidan@gmail.com>
2025-10-31 15:18:02 +0300

    add student

--- server/routes/studentRoutes.js
+++ server/routes/studentRoutes.js
@@ -9,27 +9,27 @@ const {
 } = require('../controllers/studentController');
 
 
-const { protect, authorize, canViewStudentData } = require('../middleware/authMiddleware');
+const { protect, authorize, canViewStudentData , isHomeroomTeacherOrAdmin} = require('../middleware/authMiddleware');
 
 // Import the main multer instance we just created
 const upload = require('../middleware/upload');
 
 // Standard JSON routes
 router.route('/')
-    .post(protect, authorize('admin'), createStudent)
+    .post(protect, authorize('admin' || ""), createStudent)
     .get(protect, getStudents);
 
 router.route('/:id')
     .get(canViewStudentData, getStudentById)
-    .put(protect, authorize('admin'), updateStudent)
-    .delete(protect, authorize('admin'), deleteStudent);
+    .put(protect, authorize('admin'),isHomeroomTeacherOrAdmin, updateStudent)
+    .delete(protect, authorize('admin'),isHomeroomTeacherOrAdmin, deleteStudent);
 
 // --- THE DEFINITIVE PHOTO UPLOAD ROUTE ---
 // We call upload.single() right here. This is the clearest and most direct way.
-router.post('/photo/:id', protect, authorize('admin'), upload.single('profilePhoto'), uploadProfilePhoto);
+router.post('/photo/:id', protect, isHomeroomTeacherOrAdmin,authorize('admin'), upload.single('profilePhoto'), uploadProfilePhoto);
 
 // --- The Excel upload route (we'll keep it simple for now) ---
 const localUpload = multer({ dest: 'uploads/' });
-router.post('/upload', protect, authorize('admin'), localUpload.single('studentsFile'), bulkCreateStudents);
+router.post('/upload', protect, authorize('admin'), isHomeroomTeacherOrAdmin,localUpload.single('studentsFile'), bulkCreateStudents);
 
 module.exports = router;
