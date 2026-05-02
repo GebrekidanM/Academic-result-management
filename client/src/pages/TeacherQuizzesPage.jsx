cec4c0c8a2b579e8f6ba6311af1742a152aafa1c
GebrekidanM <designerkidan@gmail.com>
2026-05-02 05:36:14 -0700

    Quiz how long remain

--- client/src/pages/TeacherQuizzesPage.jsx
+++ client/src/pages/TeacherQuizzesPage.jsx
@@ -22,9 +22,9 @@ const TeacherQuizzesPage = () => {
 
     return (
         <div className="max-w-5xl mx-auto p-4 md:p-8 bg-slate-50 min-h-screen">
-            <div className="flex justify-between items-center mb-8">
+            <div className="flex flex-row md:flex-col justify-between items-center mb-8">
                 <div>
-                    <h1 className="text-3xl font-black text-slate-800">{t('my_quizzes')}</h1>
+                    <h1 className="text-3xl font-black text-slate-800">{t('Quizzes')}</h1>
                     <p className="text-slate-500">{t('manage_your_assessments')}</p>
                 </div>
                 <button 
