3d4246a793887bca126625c82f22672ab9f9712d
GebrekidanM <designerkidan@gmail.com>
2026-06-14 23:53:58 +0300

    improvement

--- client/src/components/parent-dashboard/QuizCenter.jsx
+++ client/src/components/parent-dashboard/QuizCenter.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Quiz from "./Quiz";
-import quizService from "../../services/quizService";
+import quizService from "../../shared/services/quizService";
 
 
 const QuizCenter = ({ quizzes, quizStatuses }) => {
