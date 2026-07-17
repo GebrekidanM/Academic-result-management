dc3dced9176ebae3f70ba55207e2a0a8dffddde3
GebrekidanM <designerkidan@gmail.com>
2026-07-17 21:37:51 +0300

    filter subject 7

--- server/routes/studentRoutes.js
+++ server/routes/studentRoutes.js
@@ -3,17 +3,14 @@ const router = express.Router();
 const studentController = require('../controllers/studentController');
 const { protect } = require('../middleware/authMiddleware');
 
-// 1. STATIC ROUTES
 router.get('/bulk-end-of-year/count', protect, studentController.getBulkEndOfYearCount);
 router.put('/end-of-year', protect, studentController.bulkSetEndOfYearByEC);
 router.get('/search/:studentId', studentController.getStudentForRegistration);
 router.post('/re-register', protect, studentController.reRegisterStudent);
 
-// 2. DYNAMIC ROUTES WITH SUB-PATHS
 router.post('/photo/:id', protect, studentController.uploadProfilePhoto);
 router.post('/reset/:studentId', protect, studentController.resetPassword);
 
-// 3. GENERIC CRUD DYNAMIC ROUTES
 router.get('/', protect, studentController.getStudents);
 router.post('/', protect, studentController.createStudent);
 router.get('/:id', protect, studentController.getStudentById);
