e7b54ba53a7e90bb04112fbc1988b4513ede8a9e
GebrekidanM <designerkidan@gmail.com>
2026-08-10 10:15:35 +0300

    version 7.5

--- server/index.js
+++ server/index.js
@@ -16,7 +16,6 @@ app.use(express.urlencoded({ extended: true }));
 
 // ... existing requires
 const {performBackup} = require('./utils/backup');
-const {repairAssessmentTypeReferences} = require('./utils/repairAssessmentTypeReferences');
 // --- Routes ---
 app.use('/api/students', require('./routes/studentRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
